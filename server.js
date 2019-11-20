d35481f8371508114cb6316d00a41bb1d14fe280
Kevin Xu <karkevin@gmail.com>
2019-11-20 01:48:08 -0500

    attempt

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // get anything else besides the specified endpoints
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 const port = process.env.PORT || 5000;
