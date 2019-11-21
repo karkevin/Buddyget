8ad672e8e4d1e563d2e71be9183d087c12c39337
Kevin Xu <karkevin@gmail.com>
2019-11-21 02:34:14 -0500

    re-enforced get group authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+// import { CSSTransition } from 'react-transition-group';
+// Components
 import Layout from "../components/Layout";
 import LoginModal from "../components/modals/LoginModal";
 import RegisterModal from "../components/modals/RegisterModal";
