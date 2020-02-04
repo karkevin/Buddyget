44e6a6ccfe7595de35246f335527eeb4083a4775
Kevin Xu <karkevin@gmail.com>
2020-02-03 21:32:29 -0500

    Compress app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const compression = require("compression");
 
 const app = express();
+
+app.use(compression());
 
 // used to parse incoming requests with JSON payload.
 app.use(express.json());
