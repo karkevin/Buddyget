fedee951c4799effbc358db75feabb4a7d677eee
Kevin Xu <karkevin@gmail.com>
2020-07-09 19:49:20 -0400

    Name change

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bottom-0 w-1/4 text-left py-4 pl-5 text-md ">
-      <p className="w-20 mb-1 sm:w-full font-medium opacity-70">
+      {/* <p className="w-20 mb-1 sm:w-full font-medium opacity-70">
         Made by Kevin Xu
-      </p>
+      </p> */}
       <a
         href="https://github.com/karkevin"
         className="text-lg font-medium opacity-70 hover:font-bold transition-all transition-100"
